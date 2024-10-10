636bfb25ea041b16e8b26a582e960e37527da900
KOVID-MS <kovidmsk@gmail.com>
2024-10-10 21:08:50 +0530

    commit

--- Authtestapp/app.js
+++ Authtestapp/app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require("path");
 const bcrypt = require("bcrypt");
 const userModel = require("./models/user");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const alert = require('alert');
 const cookieParser = require("cookie-parser");
 
 app.set('view engine','ejs');
@@ -13,28 +14,63 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/",(req,res)=>{
-    res.render('home')
+    res.render('register');
 })
 
-app.post("/create",(req,res)=>{
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.get("/profile",(req,res)=>{
+    res.render("profile");
+})
+
+app.post("/create",async(req,res)=>{
     const {username,email,password} = req.body;
-    bcrypt.genSalt(10,(err,salt)=>{
-        bcrypt.hash(password,salt,async (err,hash)=>{
-            let user = await userModel.create({
-                username,
-                email,
-                password : hash,
+    const user = await userModel.findOne({email});
+    if(user){
+        res.send("Something went wrong");
+    }
+    else{
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(password,salt,async (err,hash)=>{
+                let user = await userModel.create({
+                    username,
+                    email,
+                    password : hash,
+                })
+                let token = jwt.sign(email,"secret");
+                res.cookie('token',token);
+                res.redirect("/profile");
             })
-            let token = jwt.sign(email,"secret");
-            res.cookie('token',token);
-            res.send(user);
         })
-    })
+    }
+    
+})
+
+app.post("/login",async(req,res)=>{
+    const {email,password} = req.body;
+    const data = await userModel.findOne({email});
+    if(!data) {
+        res.send('Incorrect Email/Password');
+    }
+    else{
+        bcrypt.compare(password,data.password,(err,result)=>{
+            if(result){
+                let token = jwt.sign(email,"secret");
+                res.cookie('token',token);
+                res.redirect("/profile");
+            }
+            else{
+            res.send('Incorrect Email/Password');
+            }
+        })
+    }
 })
 
 app.get("/logout",(req,res)=>{
     res.cookie('token', "");
-    res.render("home");
+    res.redirect("/");
 })
 
-app.listen(3000)
+app.listen(3000);
